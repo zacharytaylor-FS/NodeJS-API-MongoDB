bbb890a7590b611075f95c67664a47b71b8fda83
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-04-02 21:37:57 -0400

    add db functions

--- db/db.js
+++ db/db.js
@@ -3,13 +3,18 @@ require('dotenv').config()
 
 const connection = async () =>{
     try {
-        return await mongoose.connect(process.env.MONGODB), 
+        return await mongoose.connect(process.env.MONGODB_URL), 
         console.log(`Real MongoDB is up and running`)
     } catch (error) {
         console.log(error)
     }
     
 };
+
+const postBook = async (newBook) => {
+    console.log("Real Book",newBook);
+    return await newBook.save()
+}
 
 const savePost = async (newPost) => {
     console.log("Saving real posts")
@@ -20,4 +25,4 @@ const disconnect = async () => {
     await mongoose.connection.close()
 }
 
-module.exports = {connection, savePost, disconnect }
+module.exports = {connection, savePost,}
